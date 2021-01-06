9c7a88bcbe48c434bf6dae106015e96991234707
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2021-01-05 22:15:31 -0700

    Application CPS conversion seems to be working

--- passes/cps_conversion.rkt
+++ passes/cps_conversion.rkt
@@ -61,9 +61,37 @@
 
               (ast->cps (car body) k-fresh))))]
 
+    ;; Application: function needs to be converted
     [(application _ func-ref args)
-     #:when (any? (compose not simple?) args)
-     (error "unimplemented")]
+     #:when (not (simple? func-ref))
+     (ast->cps func-ref
+               (let ([k-fresh (gensym 'x₁)])
+                 (lambda-dec
+                  (list k-fresh)
+                  '()
+                  (ast->cps (application k-fresh args) k₀))))]
+
+    ;; Application: function has been converted; now convert args
+    [(application _ func-ref args)
+     #:when (simple? func-ref)
+     
+     (define (conv-args simple rst)
+       (if (null? rst)
+           (application func-ref (append (reverse simple) (list k₀)))
+           (if (simple? (car rst))
+               (conv-args (cons (car rst) simple) (cdr rst))
+               ;; Ok, need to convert this argument
+               (ast->cps
+                (car rst)
+                (let ([k-fresh (gensym 'x₂)])
+                  (lambda-dec
+                   (list k-fresh)
+                   (ast->cps
+                    (application func-ref
+                                 `(,@(reverse simple) ,k-fresh ,@rst))
+                    k₀)))))))
+
+     (conv-args '() args)]
 
     [_ (error "Unable to match expression in CPS conversion" expr)]))
 
